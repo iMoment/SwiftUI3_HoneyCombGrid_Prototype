ea550f86efa0f5a4ba3006657ec11e2e222bd334
Stanley Pan <imoment.developer@gmail.com>
2022-02-16 15:22:08 -0500

    Initial Commit

---
+++ HoneyCombUI/HoneyCombUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyCombUIApp.swift
+//  HoneyCombUI
+//
+//  Created by Stanley Pan on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct HoneyCombUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
